050c45d9282902517138e9ab290889b77afa76c1
John Doe <sergey@itikho.com>
2023-12-13 22:18:41 +0300

    Initial Commit

---
+++ SUITrafficLight/SUITrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUITrafficLightApp.swift
+//  SUITrafficLight
+//
+//  Created by John Doe on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SUITrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
